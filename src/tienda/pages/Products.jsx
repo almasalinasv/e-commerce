32e12c9e7f707b79ea413d26808248b2e59fd434
almasalinasv <alwayskidrauhl04sv@gmail.com>
2023-05-15 21:40:46 -0600

    filter added

--- src/tienda/pages/Products.jsx
+++ src/tienda/pages/Products.jsx
@@ -7,26 +7,35 @@ import { GetImages } from "../components/GetImages";
 export const Products = () => {
 
 const {data:images, isLoading} = useFetchProducts('products');
+const [filter, setFilter] = useState(images);
+const filterProduct = (cat) => {
+  const filtrar = images.filter((image) => image.category === cat);
+  setFilter(filtrar);
+
+}
 
 return(
   <>
 
     <div className="container mt-5">
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 text-center">
         
 
           <h1 className="text-center">Nuestros Productos</h1>
-          <div className="container p-2 text-center">
-        
-          </div>
+          <button className="btn btn-outline-secondary m-3" onClick={() => setFilter(images)}>Todo</button>
+          <button className="btn btn-outline-secondary m-3" onClick={() => filterProduct("electronics")}>Electrónicos</button>
+            <button className="btn btn-outline-secondary m-3" onClick={() => filterProduct("jewelery")}>Joyería</button>
+            <button className="btn btn-outline-secondary m-3" onClick={() => filterProduct("men's clothing")}>Ropa de hombre</button>
+            <button className="btn btn-outline-secondary m-3" onClick={() => filterProduct("women's clothing")}>Ropa de mujer</button>
+
           <div className="card-grid">
             {
               isLoading && <h2>Cargando ...</h2>
             }
 
             {
-              images.map((image) => (
+              filter.map((image) => (
                 <GetImages key={image.id}{...image}/>
               ))
             }
